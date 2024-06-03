e5e55c23a211542f3883c72150c2f52dfe219ce9
Jet <taijunjet@hotmail.com>
2024-06-03 23:51:30 +0900

    update desktop install

--- term-install.sh
+++ term-install.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-# sshkey for git
-echo "Performing ssh keygen using ed25519 for git..."
-ssh-keygen -t ed25519 -C $hostname -f /.ssh/ed_25519 -q -N ""
+# conditionally generate ssh keys
+read -p "Generate new ssh keys? (y/n): " gen_keys
+if [ "$user_input" = "y" ]; then
+  # sshkey for git
+  echo "Performing ssh keygen using ed25519 for git..."
+  ssh-keygen -t ed25519 -C $hostname -f /.ssh/ed_25519 -q -N ""
 
-# print out the ssh public key to be added to git manually
-echo "--------------------------------------------"
-cat ~/.ssh/ed_25519.pub
-echo "--------------------------------------------"
-echo "Please add the above ssh key to https://github.com/settings/ssh/new"
-echo "Press ENTER when done..."
-read -l _
+  # print out the ssh public key to be added to git manually
+  echo "--------------------------------------------"
+  cat ~/.ssh/ed_25519.pub
+  echo "--------------------------------------------"
+  echo "Please add the above ssh key to https://github.com/settings/ssh/new"
+  echo "Press ENTER when done..."
+  read -l _
+else
+  echo "Skipping ssh keygen..."
+fi
 
 # install git
 echo "Installing git..."
+sudo apt update
 sudo apt install git
+
+# clean our install directory
+rm -rf kitty
+rm -rf nvim
+rm -rf tmux
+rm -rf fish
 
 # install all my dotfiles, this installs things using HTTPs so no ssh
 echo "Cloning all dotfiles"
